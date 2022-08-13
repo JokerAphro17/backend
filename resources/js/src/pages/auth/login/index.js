520b899fcc6d9d3d38f1f7549074aea3e9e56e71
Dimanchecfa <dimanchenamo@gmail.com>
2022-08-13 15:24:30 +0000

    LoginForm

--- resources/js/src/pages/auth/login/index.js
+++ resources/js/src/pages/auth/login/index.js
@@ -34,6 +34,7 @@ const Login = () => {
                                     onBlur={onBlur}
                                     helperText={errors.email && errors.email.message}
                                 />)}
+                                
                                 rules={{ required: "Email est requis",
                                     pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: "Adress email invalide" },
                                 }}
